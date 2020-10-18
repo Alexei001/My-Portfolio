e08602b20088ca066e68bc9fd8a450e5a88b3aab
Alexei001 <alexeiyoyo12@gmail.com>
2020-10-18 16:34:04 +0300

    update deleted files

--- StrAPI/test_project_back_end/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0559dc5e5f8eff77606457a89abaa648'),
-    },
-  },
-});
--- StrAPI/test_project_back_end/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '239eee43-7dcd-4507-91f0-c2292cab3156'
-};
